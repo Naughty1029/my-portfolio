edbb4c8cad7f87ae1a21f74315933a8bc857f0c3
渡邉雅広 <watanabemasahiro@masahiro.local>
2022-03-12 16:48:33 +0900

    eslint prettier husky追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,17 +14,9 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public_html/')
     .ts('resources/ts/index.tsx', 'public_html/js')
     .sass('resources/sass/app.scss', 'public_html/css')
-    .sourceMaps()
-    .browserSync({
-        host: 'my-portfolio.test/', // proxy.targetと合わせる
-        proxy: {
-            target: 'http://my-portfolio.test', // /etc/hostsで設定しているドメインのURL
-        },
-        files: [ // 変更検知してほしいファイルを列挙する
-            './resources/**/*',
-            './public_html/**/*',
-        ]
-    });
+    .sourceMaps();
+
+mix.browserSync('my-portfolio.test');
 
 if (mix.inProduction()) {
     mix.version();
